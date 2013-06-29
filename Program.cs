e2885178432106d4263fd1d2352922543b76a0f8
Jerome Cheng <jcheng31@gmail.com>
2013-06-29 18:59:23 +0800

    Use text instead of bitmaps for number display.

--- Program.cs
+++ Program.cs
@@ -97,40 +97,13 @@ namespace AgentIntervals
 
         static void DrawSeconds(int digit)
         {
-            int onesDigit = digit % 10;
-            int tensDigit = digit / 10;
-            _display.DrawImage(64, 32, GetDigit(onesDigit), 12, 0, 40, 64);
-            _display.DrawImage(24, 32, GetDigit(tensDigit), 12, 0, 40, 64);
-        }
+            Font fontConsolaMonoBold32 = Resources.GetFont(Resources.FontResources.ConsolaMonoBold32);
 
-        static Bitmap GetDigit(int digitNumber)
-        {
-            switch (digitNumber)
-            {
-                case 0:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._0), Bitmap.BitmapImageType.Gif);
-                case 1:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._1), Bitmap.BitmapImageType.Gif);
-                case 2:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._2), Bitmap.BitmapImageType.Gif);
-                case 3:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._3), Bitmap.BitmapImageType.Gif);
-                case 4:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._4), Bitmap.BitmapImageType.Gif);
-                case 5:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._5), Bitmap.BitmapImageType.Gif);
-                case 6:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._6), Bitmap.BitmapImageType.Gif);
-                case 7:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._7), Bitmap.BitmapImageType.Gif);
-                case 8:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._8), Bitmap.BitmapImageType.Gif);
-                case 9:
-                    return new Bitmap(Resources.GetBytes(Resources.BinaryResources._9), Bitmap.BitmapImageType.Gif);
-                default:
-                    return null;
-            }
-        }
+            int tens = digit/10;
+            int ones = digit%10;
 
+            String digitText = tens.ToString() + ones.ToString();
+            _display.DrawText(digitText, fontConsolaMonoBold32, Color.White, 40, 32);
+        }
     }
 }
