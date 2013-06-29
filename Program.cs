f12a2e5a12774f19fcdd3d19cbd3f3642beba9ae
Jerome Cheng <jcheng31@gmail.com>
2013-06-29 19:17:40 +0800

    Add ability to reset current interval using down button.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,13 @@ namespace AgentIntervals
         private const int ShortIntervalTime = 10;
 
         private static int _secondsLeft = ShortIntervalTime;
-        private static IntervalType _type;
+        private static IntervalType _currentIntervalType;
+
+        private static InterruptPort _upButton;
+        private static InterruptPort _selectButton;
+        private static InterruptPort _downButton;
+
+        private static Timer _countdownTimer;
 
         enum IntervalType
         {
@@ -30,10 +36,45 @@ namespace AgentIntervals
             // initialize display buffer
             _display = new Bitmap(Bitmap.MaxWidth, Bitmap.MaxHeight);
 
-            Timer secondTimer = new Timer(SecondTimerCallback, null, 0, 1000);
+            // Keep references to the buttons.
+            _upButton = new InterruptPort(HardwareProvider.HwProvider.GetButtonPins(Button.VK_UP), false, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeLow);
+            _selectButton = new InterruptPort(HardwareProvider.HwProvider.GetButtonPins(Button.VK_SELECT), false, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeLow);
+            _downButton = new InterruptPort(HardwareProvider.HwProvider.GetButtonPins(Button.VK_DOWN), false, Port.ResistorMode.PullDown, Port.InterruptMode.InterruptEdgeLow);
+
+            _downButton.OnInterrupt += ResetCounter;
+
+            ResetTimer();
 
             // go to sleep; all further code should be timer-driven or event-driven
             Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void ResetCounter(uint data1, uint data2, DateTime time)
+        {
+            switch (_currentIntervalType)
+            {
+                case IntervalType.LongInterval:
+                    _secondsLeft = LongIntervalTime;
+                    break;
+                case IntervalType.MediumInterval:
+                    _secondsLeft = MediumIntervalTime;
+                    break;
+                case IntervalType.ShortInterval:
+                    _secondsLeft = ShortIntervalTime;
+                    break;
+            }
+
+            DrawDisplay(_secondsLeft);
+            ResetTimer();
+        }
+
+        private static void ResetTimer()
+        {
+            if (_countdownTimer != null)
+            {
+                _countdownTimer.Dispose();
+            }
+            _countdownTimer = new Timer(SecondTimerCallback, null, 0, 1000);
         }
 
         private static void SecondTimerCallback(object state)
@@ -43,9 +84,9 @@ namespace AgentIntervals
 
             if (_secondsLeft <= 0)
             {
-                _type = (IntervalType)((int)(_type + 1) % 3);
+                _currentIntervalType = (IntervalType)((int)(_currentIntervalType + 1) % 3);
 
-                switch (_type)
+                switch (_currentIntervalType)
                 {
                     case IntervalType.LongInterval:
                         _secondsLeft = LongIntervalTime;
@@ -77,7 +118,7 @@ namespace AgentIntervals
             Font fontNinaB = Resources.GetFont(Resources.FontResources.NinaB);
             String intervalType = null;
             int leftMargin = 20;
-            switch (_type)
+            switch (_currentIntervalType)
             {
                 case IntervalType.LongInterval:
                     intervalType = "Low Intensity";
